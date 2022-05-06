1141b00cb53ac89f921e068b6f36c85c72c44517
Chris <chris.hoffmann84@gmail.com>
2022-05-06 23:07:39 +0200

    basic listener added

--- cache/cache.go
+++ cache/cache.go
@@ -1,5 +1,11 @@
 package cache
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 type Cache struct {
 	Name           string
 	UrlScheme      string
@@ -7,9 +13,55 @@ type Cache struct {
 }
 
 func New(name string, urlScheme string, TimeToLiveDays int) Cache {
-	return Cache{
+	c := Cache{
 		Name:           name,
 		UrlScheme:      urlScheme,
 		TimeToLiveDays: TimeToLiveDays,
 	}
+
+	http.HandleFunc("/osm/", c.Serve)
+
+	return c
+}
+
+/*
+func (c *Cache) request(x string, y string, z string, s string) {
+
+}
+
+func (c *Cache) load(x string, y string, z string, s string) {
+
+}
+
+func (c *Cache) save(x string, y string, z string, s string) {
+
+}
+*/
+
+func (c *Cache) Serve(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Println("req.RequestURI" + req.RequestURI)
+
+	requestUri := strings.Split(req.RequestURI, "/")
+
+	if len(requestUri) < 6 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Bad Request"))
+		return
+	}
+
+	s := requestUri[2]
+	x := requestUri[3]
+	y := requestUri[4]
+	z := requestUri[5]
+
+	fmt.Printf("Found: s=[%s], x=[%s], y=[%s], z=[%s]\n", s, x, y, z)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte("Hello Cache!"))
 }
--- go-maptilecache-example.go
+++ go-maptilecache-example.go
@@ -1,3 +1,16 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Christian1984/go-maptilecache/cache"
+)
+
+func main() {
+	httpListen := "0.0.0.0:9001"
+	cache.New("osm", "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 1)
+
+	fmt.Println("Map Tile Cache listening at " + httpListen)
+	http.ListenAndServe(httpListen, nil)
+}
