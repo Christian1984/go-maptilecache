7bdbb7aa9985c67ae306bd5f530ab6f6303de0f8
Chris <chris.hoffmann84@gmail.com>
2022-05-07 16:55:52 +0200

    added basic client implementation, results in 403 error, though

--- cache/cache.go
+++ cache/cache.go
@@ -1,34 +1,80 @@
 package cache
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Cache struct {
-	Name           string
-	UrlScheme      string
-	TimeToLiveDays int
+	Route      []string
+	UrlScheme  string
+	TimeToLive time.Duration
+	ApiKey     string
 }
 
-func New(name string, urlScheme string, TimeToLiveDays int) Cache {
+func New(route []string, urlScheme string, TimeToLiveDays time.Duration, apiKey string) (Cache, error) {
 	c := Cache{
-		Name:           name,
-		UrlScheme:      urlScheme,
-		TimeToLiveDays: TimeToLiveDays,
+		Route:      route,
+		UrlScheme:  urlScheme,
+		TimeToLive: TimeToLiveDays,
+		ApiKey:     apiKey,
 	}
 
-	http.HandleFunc("/osm/", c.Serve)
+	if len(route) < 1 {
+		return c, errors.New("Could not initialize Cache, reason: Route invalid, must have at least one entry!")
+	}
 
-	return c
+	routeString := strings.Join(route, "/")
+
+	http.HandleFunc("/"+routeString+"/", c.serve)
+	fmt.Println("New Cache initialized on route /" + routeString + "/")
+
+	return c, nil
+}
+
+func log(message string) {
+	fmt.Println(message)
+}
+
+func (c *Cache) request(x string, y string, z string, s string) {
+	url := c.UrlScheme
+	url = strings.Replace(url, "{s}", s, 1)
+	url = strings.Replace(url, "{x}", x, 1)
+	url = strings.Replace(url, "{y}", y, 1)
+	url = strings.Replace(url, "{z}", z, 1)
+
+	log("Requesting tile from " + url)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log("Could not request tile, reason: " + err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log("Could not request tile, bad status code: " + strconv.Itoa(resp.StatusCode))
+		return
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log("Could parse response body, reason: " + err.Error())
+		return
+	}
+
+	log("Received " + strconv.Itoa(len(bodyBytes)) + " from " + url)
 }
 
 /*
-func (c *Cache) request(x string, y string, z string, s string) {
-
-}
-
 func (c *Cache) load(x string, y string, z string, s string) {
 
 }
@@ -38,24 +84,26 @@ func (c *Cache) save(x string, y string, z string, s string) {
 }
 */
 
-func (c *Cache) Serve(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Println("req.RequestURI" + req.RequestURI)
+func (c *Cache) serve(w http.ResponseWriter, req *http.Request) {
+	// route format: /{route}/{s}/{z}/{y}/{x}/
+	log("Received request with RequestURI: [" + req.RequestURI + "]")
 
 	requestUri := strings.Split(req.RequestURI, "/")
 
-	if len(requestUri) < 6 {
+	if len(requestUri) < 5+len(c.Route) {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Bad Request"))
 		return
 	}
 
-	s := requestUri[2]
-	x := requestUri[3]
-	y := requestUri[4]
-	z := requestUri[5]
+	s := requestUri[1+len(c.Route)]
+	z := requestUri[2+len(c.Route)]
+	y := requestUri[3+len(c.Route)]
+	x := requestUri[4+len(c.Route)]
 
-	fmt.Printf("Found: s=[%s], x=[%s], y=[%s], z=[%s]\n", s, x, y, z)
+	log("Params found: s=[" + s + "], x=[" + s + "], y=[" + s + "], z=[" + s + "]")
+
+	c.request(x, y, z, s)
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
--- go-maptilecache-example.go
+++ go-maptilecache-example.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Christian1984/go-maptilecache/cache"
 )
 
+func testRequest() {
+	http.Get("http://localhost:9001/maptilecache/osm/a/6/10/10/")
+}
+
 func main() {
 	httpListen := "0.0.0.0:9001"
-	cache.New("osm", "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 1)
+	cache.New([]string{"maptilecache", "osm"}, "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 1*time.Hour, "")
 
 	fmt.Println("Map Tile Cache listening at " + httpListen)
-	http.ListenAndServe(httpListen, nil)
+	go http.ListenAndServe(httpListen, nil)
+	time.Sleep(5 * time.Second)
+	testRequest()
+
+	fmt.Println("Press Enter Key to quit")
+	fmt.Scanln()
 }
